13dbc76e45cff7095373e15a7dc441c440daed88
Your Name <you@example.com>
2021-07-27 18:35:31 -0400

    initial

---
+++ src/main/java/com/ahmad/new/com/ahmad/new/App.java
@@ -0,0 +1,13 @@
+package com.ahmad.new.com.ahmad.new;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
